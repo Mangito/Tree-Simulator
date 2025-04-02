69560ad1abb788434a949a40b655dfc8bb8187e8
Flávio Silva <flavioandre.2001.9.7@gmail.com>
2025-04-03 00:46:46 +0100

    feat: add translations and implemente with internationalization

--- globals/GameManager.gd
+++ globals/GameManager.gd
@@ -10,3 +10,27 @@ var settings = {
 	"fullscreen": false,
 	"brightness": 1,
 }
+
+func _ready() -> void:
+	set_language(get_language_index(), false)
+	set_fullscreen(settings.fullscreen, false)
+	set_brightness(settings.brightness, false)
+
+func get_language_index(lang = settings.language) -> int:
+	return LANGUAGES.keys().find(lang)
+
+func set_language(value: int, save: bool = true) -> void:
+	var key = LANGUAGES.find_key(value)
+	settings.language = key
+	TranslationServer.set_locale(key)
+	if (save): GameStorage.save_progress()
+
+func set_fullscreen(value: bool, save: bool = true) -> void:
+	settings.fullscreen = value
+	var wMode = DisplayServer.WINDOW_MODE_FULLSCREEN if value else DisplayServer.WINDOW_MODE_WINDOWED
+	DisplayServer.window_set_mode(wMode)
+	if (save): GameStorage.save_progress()
+
+func set_brightness(value: float, save: bool = true) -> void:
+	settings.brightness = value
+	if (save): GameStorage.save_progress()
--- menus/settings_menu.gd
+++ menus/settings_menu.gd
@@ -5,23 +5,18 @@ extends CanvasLayer
 @onready var bright_slider: HSlider = $ButtonsContainer/BrightnessContainer/BrightSlider
 
 func _ready() -> void:
-	lang_option_button.selected = GameManager.LANGUAGES.keys().find(GameManager.settings.language)
+	lang_option_button.selected = GameManager.get_language_index()
 	full_screen_check_button.button_pressed = GameManager.settings.fullscreen
 	bright_slider.value = GameManager.settings.brightness
 
 func _on_lang_option_button_item_selected(index: int) -> void:
-	GameManager.settings.language = GameManager.LANGUAGES.find_key(index)
-	GameStorage.save_progress()
+	GameManager.set_language(index)
 
 func _on_full_screen_check_button_toggled(toggled_on: bool) -> void:
-	GameManager.settings.fullscreen = toggled_on
-	var wMode = DisplayServer.WINDOW_MODE_FULLSCREEN if toggled_on else DisplayServer.WINDOW_MODE_WINDOWED
-	DisplayServer.window_set_mode(wMode)
-	GameStorage.save_progress()
+	GameManager.set_fullscreen(toggled_on)
 
-func _on_bright_slider_drag_ended(value_changed: bool) -> void:
-	GameManager.settings.brightness = value_changed
-	GameStorage.save_progress()
+func _on_bright_slider_value_changed(value: float) -> void:
+	GameManager.set_brightness(value)
 
 func _on_back_button_pressed() -> void:
 	hide()
