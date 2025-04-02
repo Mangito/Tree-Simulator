a796b46ce1b4affb37321eabcc17204c6cd3c6f4
Flávio Silva <flavioandre.2001.9.7@gmail.com>
2025-04-03 00:51:17 +0100

    refactor: removed brightness

--- globals/GameManager.gd
+++ globals/GameManager.gd
@@ -8,13 +8,11 @@ enum LANGUAGES {
 var settings = {
 	"language": LANGUAGES.EN,
 	"fullscreen": false,
-	"brightness": 1,
 }
 
 func _ready() -> void:
 	set_language(get_language_index(), false)
 	set_fullscreen(settings.fullscreen, false)
-	set_brightness(settings.brightness, false)
 
 func get_language_index(lang = settings.language) -> int:
 	return LANGUAGES.keys().find(lang)
@@ -29,8 +27,4 @@ func set_fullscreen(value: bool, save: bool = true) -> void:
 	settings.fullscreen = value
 	var wMode = DisplayServer.WINDOW_MODE_FULLSCREEN if value else DisplayServer.WINDOW_MODE_WINDOWED
 	DisplayServer.window_set_mode(wMode)
-	if (save): GameStorage.save_progress()
-
-func set_brightness(value: float, save: bool = true) -> void:
-	settings.brightness = value
 	if (save): GameStorage.save_progress()
--- menus/settings_menu.gd
+++ menus/settings_menu.gd
@@ -2,21 +2,16 @@ extends CanvasLayer
 
 @onready var lang_option_button: OptionButton = $ButtonsContainer/LanguageContainer/LangOptionButton
 @onready var full_screen_check_button: CheckButton = $ButtonsContainer/FullScreenContainer/FullScreenCheckButton
-@onready var bright_slider: HSlider = $ButtonsContainer/BrightnessContainer/BrightSlider
 
 func _ready() -> void:
 	lang_option_button.selected = GameManager.get_language_index()
 	full_screen_check_button.button_pressed = GameManager.settings.fullscreen
-	bright_slider.value = GameManager.settings.brightness
 
 func _on_lang_option_button_item_selected(index: int) -> void:
 	GameManager.set_language(index)
 
 func _on_full_screen_check_button_toggled(toggled_on: bool) -> void:
 	GameManager.set_fullscreen(toggled_on)
-
-func _on_bright_slider_value_changed(value: float) -> void:
-	GameManager.set_brightness(value)
 
 func _on_back_button_pressed() -> void:
 	hide()
